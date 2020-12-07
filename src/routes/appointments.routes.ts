997535ac1a473c7e17b8361de2067a6e3ab2695f
wiliancalora <wiliancalora@evoluaeducacao.com.br>
2020-12-07 05:21:47 -0300

    first commit

---
+++ src/routes/appointments.routes.ts
@@ -0,0 +1,36 @@
+import  {response, Router} from 'express';
+import { startOfHour, parseISO } from 'date-fns';
+
+import AppointmentRepository from '../repositories/AppointmentsRepository'
+
+const appointmentsRouter = Router();
+const appointmentRepository = new AppointmentRepository();
+
+appointmentsRouter.get('/', (request, response) =>{
+  const appointments = appointmentRepository.all();
+
+  return response.json(appointments);
+})
+
+appointmentsRouter.post('/', (request, response) => {
+
+  const { provider, date} = request.body;
+
+  const parsedDate = startOfHour(parseISO(date));
+
+  const findAppointmentInSameDate = appointmentRepository.findByDate(parsedDate);
+
+  if(findAppointmentInSameDate){
+    return response.status(400)
+                   .json({message : 'This appointment is already booked'});
+
+  }
+
+  const appointment = appointmentRepository.create({
+    provider,
+    date: parsedDate});
+
+  return response.json(appointment);
+});
+
+export default appointmentsRouter;
